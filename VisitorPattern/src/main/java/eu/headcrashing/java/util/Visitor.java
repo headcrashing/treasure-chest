bf4ee4500aac973cc4abd8d8d8f7d8c7da5dcd8f
Markus KARG <markus@headcrashing.eu>
2012-10-02 18:57:50 +0200

    Initial draft of generic Visitor pattern

---
+++ VisitorPattern/src/main/java/eu/headcrashing/java/util/Visitor.java
@@ -0,0 +1,34 @@
+package eu.headcrashing.java.util;
+
+/*
+ * #%L
+ * Visitor Pattern
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 Head Crashing Informatics
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * A generic visitor, able to visit visitable objects of <T> kind.
+ * 
+ * @author Markus KARG (markus@headcrashing.eu)
+ */
+public interface Visitor<T> {
+	public <U extends Visitable<T>> void visit(U visitable);
+}
